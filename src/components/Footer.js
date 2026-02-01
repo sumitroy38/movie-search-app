2d4c5bf683cae521634ba0a0c0d8eaf40cb9a95e
Sumit M Roy <146123641+sumitroy38@users.noreply.github.com>
2026-02-01 18:46:53 +0530

    Update Footer 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 const Footer = () => (
   <footer className="footer">
-    <p className="footer__text">Build with<span className="footer__accent">Thrill ⚡</span> &nbsp; . &nbsp; Engineered by : SMR</p>
+    <p className="footer__text">Build with<span className Thrill ⚡&nbsp; . &nbsp; Engineered by : <span className="footer__accent">SMR</span></p>
   </footer>
 );
 export default Footer;
